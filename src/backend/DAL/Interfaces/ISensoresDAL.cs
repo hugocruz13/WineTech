5bc918a0adc9b98fc2474981014c8608c46c6aa7
DaniC3101 <a23016@alunos.ipca.pt>
2025-12-17 17:42:15 +0000

    Sensores Camada SOAP

---
+++ src/backend/DAL/Interfaces/ISensoresDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ISensoresDAL
+    {
+        Task<Models.Sensores> InserirSensor(Models.Sensores sensor);
+        Task<List<Models.Sensores>> TodosSensores();
+        Task<List<Models.Sensores>> ObterSensoresPorAdega(int adegaId);
+        Task<List<Models.Leituras>> ObterLeiturasPorSensor(int sensorId);
+
+    }
+}
